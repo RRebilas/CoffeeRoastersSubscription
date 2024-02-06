cfce95511a5317f875319804f0ac3216ab24d3ac
Rafał Rębilas <rrafal7@gmail.com>
2024-02-06 21:57:31 +0100

    Update assets & add fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,10 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./src/**/*.{ts, html}'],
   theme: {
+    fontFamily: {
+      fraunces: ['Fraunces', 'serif'],
+      barlow: ['Barlow', 'serif'],
+    },
     colors: {
       'dark-cyan': '#0E8784',
       'dark-grey-blue': '#333D4B',
@@ -15,7 +19,7 @@ export default {
       base: ['1.6rem', { lineHeight: '2.6rem' }],
       xl: ['2.4rem', { lineHeight: '3.2rem' }],
       '2xl': ['3.2rem', { lineHeight: '3.6rem' }],
-      '3xl': ['40rem', { lineHeight: '4.8rem' }],
+      '3xl': ['4rem', { lineHeight: '4.8rem' }],
       '4xl': ['7.2rem', { lineHeight: '7.2rem' }],
       '5xl': ['15rem', { lineHeight: '7.2rem' }],
     },
